6521b54f754029e709d20d50f3ee358d9b1c400e
Charles Aroma <charlesaroma9@gmail.com>
2025-03-08 19:19:09 +0300

    Faculty Student Profile

---
+++ src/pages/StudentsManagement/SToolTip.js
@@ -0,0 +1,9 @@
+const SToolTip = {
+  "School of Business And Management": "SBM",
+  "School of Distance Learning And Information Technology": "SDLIT",
+  "School of Civil Service, Public Administration And Governance": "SCPAG",
+  "School of Management Sciences": "SMS",
+  "Regional Centres": "RC",
+};
+
+export default SToolTip;
