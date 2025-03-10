efa1f6661e7b8f22a4c82ed31ef4762c5a3382ea
Charles Aroma <charlesaroma9@gmail.com>
2025-03-10 04:17:19 +0300

    Sidebar IMG

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const user = {
     name: 'Charles Aroma',
     role: 'IT Administrator',
-    avatar: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    avatar: 'https://images.unsplash.com/photo-1531384441138-2736e62e0919?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
   };
 
   const menuItems = [
